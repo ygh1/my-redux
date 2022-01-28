7ffbf4e251486084d6f7d316852082f3c237aa9a
yanguanghao <yanguanghao@qihangxingchen.com>
2022-01-28 12:00:14 +0800

    添加 reducer校验

--- createStore.js
+++ createStore.js
@@ -1,9 +1,15 @@
 export default function createStore (reducer, preloadedState) {
+  if (typeof reducer !== 'function') {
+    throw new Error('reducer must be a function.')
+  }
+
+  // 保存store
   let currentState = preloadedState
 
   // 存放订阅者函数
   let currentListeners = []
 
+  // 获取state，闭包保存状态
   function getState () {
     return currentState
   }
