7e95dcf598267ae2ccad9bf820255c115eea69e9
yanguanghao <yanguanghao@qihangxingchen.com>
2022-01-28 17:14:12 +0800

    导出

--- redux.js
+++ redux.js
@@ -1,3 +1,4 @@
 export { default as createStore } from "./createStore.js"
 export { default as appleMiddleware } from "./appleMiddleware.js"
 export { default as bindActionCreators } from "./bindActionCreators.js"
+export { default as combineReducers } from "./combineReducers.js"
